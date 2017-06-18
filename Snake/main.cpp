e6d33d57a4ca07421dcc857fa0180c7bec76397f
Kerndog73 <kerndog73@gmail.com>
2017-06-18 17:44:12 +0930

    Remove swift stuff

---
+++ Snake/main.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  Snake
+//
+//  Created by Indi Kernick on 18/6/17.
+//  Copyright © 2017 Indi Kernick. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int, char **) {
+  std::cout << "Sup world\n";
+}
