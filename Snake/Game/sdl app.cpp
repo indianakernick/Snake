5b94d5d7e20cdf6823f855a70d215c0068fef895
Kerndog73 <kerndog73@gmail.com>
2017-06-21 12:58:30 +0930

    Add App and SDLApp

---
+++ Snake/Game/sdl app.cpp
@@ -0,0 +1,14 @@
+//
+//  sdl app.cpp
+//  Snake
+//
+//  Created by Indi Kernick on 21/6/17.
+//  Copyright © 2017 Indi Kernick. All rights reserved.
+//
+
+#include "sdl app.hpp"
+
+SDLApp::SDLApp(const Platform::Window::Desc &windowDesc, const bool vsync)
+  : lib(),
+    window(lib.makeWindow(windowDesc)),
+    renderer(lib.makeRenderer(window, vsync)) {}
--- Snake/main.cpp
+++ Snake/main.cpp
@@ -6,8 +6,9 @@
 //  Copyright © 2017 Indi Kernick. All rights reserved.
 //
 
-#include <SDL2/SDL.h>
-#include "Engine/Math/dir.hpp"
+#include "Game/sdl app.hpp"
+#include <Simpleton/Math/dir.hpp>
+#include <Simpleton/Time/freq limiter.hpp>
 #include <deque>
 #include <glm/vec4.hpp>
 #include <vector>
@@ -22,85 +23,92 @@ const Color BACK_COLOR = {0, 0, 0, 0};
 const Color SNAKE_COLOR = {255, 255, 255, 255};
 const Color FOOD_COLOR = {0, 255, 0, 255};
 
-const size_t UPDATES_PER_SECOND = 8;
-const size_t FRAMES_PER_UPDATE = 60 / UPDATES_PER_SECOND;
+const uint64_t UPDATES_PER_SECOND = 8;
 
-const Pos GAME_SIZE = {35, 35};
-const Pos WINDOW_SIZE = {700, 700};
-const glm::tvec2<float> PX_SIZE = {
-  static_cast<float>(WINDOW_SIZE.x) / GAME_SIZE.x,
-  static_cast<float>(WINDOW_SIZE.y) / GAME_SIZE.y
+const Platform::Window::Desc WINDOW_DESC = {
+  "Snake",
+  {700, 700},
+  true
 };
 
-void setColor(SDL_Renderer *, Color);
-Math::Dir getNextDir(SDL_Scancode, Math::Dir);
-Pos getFoodPos(const std::deque<Pos> &);
-void renderSnake(SDL_Renderer *, const std::deque<glm::ivec2> &);
-void renderFood(SDL_Renderer *, Pos);
+const Pos GAME_SIZE = {35, 35};
 
-int main(int, char **) {
-  SDL_Init(SDL_INIT_EVERYTHING);
-  SDL_Window *window = SDL_CreateWindow(
-    "Snake",
-    SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-    WINDOW_SIZE.x, WINDOW_SIZE.y,
-    SDL_WINDOW_SHOWN
-  );
-  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-  
-  Math::Dir currentDir = Math::Dir::LEFT;
-  Math::Dir nextDir = currentDir;
-  const Pos center = GAME_SIZE / 2;
-  std::deque<Pos> snake = {{center.x - 1, center.y}, center, {center.x + 1, center.y}};
-  size_t frame = 0;
-  bool quit = false;
-  Pos food = getFoodPos(snake);
-  while (!quit) {
-    SDL_Event e;
-    while (SDL_PollEvent(&e)) {
-      switch (e.type) {
-        case SDL_QUIT:
-          quit = true;
-          break;
-        case SDL_KEYDOWN:
-          nextDir = getNextDir(e.key.keysym.scancode, currentDir);
-          break;
-      }
-    }
-    
-    if (frame % FRAMES_PER_UPDATE == 0) {
-      snake.push_front(snake.front() + ToVec::conv(nextDir));
-      snake.front() = (snake.front() + GAME_SIZE) % GAME_SIZE;
-      if (snake.front() == food) {
-        food = getFoodPos(snake);
-      } else {
-        snake.pop_back();
-      }
-      currentDir = nextDir;
-    }
-    
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
-    SDL_RenderClear(renderer);
-    renderSnake(renderer, snake);
-    renderFood(renderer, food);
-    SDL_RenderPresent(renderer);
-    
-    frame++;
+class AppImpl final : public SDLApp {
+public:
+  AppImpl()
+    : SDLApp(WINDOW_DESC, true),
+      freqLimiter(Time::OP_PER_SEC, UPDATES_PER_SECOND) {
+    const Pos center = GAME_SIZE / 2;
+    snake = {{center.x - 1, center.y}, center, {center.x + 1, center.y}};
   }
   
-  SDL_DestroyRenderer(renderer);
-  SDL_DestroyWindow(window);
-  SDL_Quit();
+private:
+  Time::DeltaFreqLimiter<uint64_t> freqLimiter;
+  std::deque<Pos> snake;
+  Pos food;
+  Math::Dir currentDir = Math::Dir::LEFT;
+  Math::Dir nextDir = Math::Dir::LEFT;
+
+  void init() override;
+  bool input(uint64_t) override;
+  bool update(uint64_t) override;
+  void render(uint64_t) override;
+  
+  void setNextDir(SDL_Scancode);
+  void setFoodPos();
+  
+  void renderSnake();
+  void renderFood();
+};
+
+int main(int, char **) {
+  AppImpl app;
+  app.mainLoop();
   
   return 0;
 }
 
-void setColor(SDL_Renderer *renderer, const Color color) {
-  SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
+void AppImpl::init() {
+  SDL_RenderSetLogicalSize(renderer, GAME_SIZE.x, GAME_SIZE.y);
 }
 
-Math::Dir getNextDir(const SDL_Scancode code, const Math::Dir currentDir) {
-  Math::Dir nextDir = currentDir;
+bool AppImpl::input(const uint64_t) {
+  SDL_Event e;
+  while (SDL_PollEvent(&e)) {
+    switch (e.type) {
+      case SDL_QUIT:
+        return false;
+      case SDL_KEYDOWN:
+        setNextDir(e.key.keysym.scancode);
+        break;
+    }
+  }
+  return true;
+}
+
+bool AppImpl::update(const uint64_t delta) {
+  freqLimiter.advance(delta);
+  if (freqLimiter.canDo()) {
+    snake.push_front(snake.front() + ToVec::conv(nextDir));
+    snake.front() = (snake.front() + GAME_SIZE) % GAME_SIZE;
+    if (snake.front() == food) {
+      setFoodPos();
+    } else {
+      snake.pop_back();
+    }
+    currentDir = nextDir;
+  }
+  return true;
+}
+
+void AppImpl::render(const uint64_t) {
+  renderer.clear();
+  renderSnake();
+  renderFood();
+  renderer.present();
+}
+
+void AppImpl::setNextDir(const SDL_Scancode code) {
   switch (code) {
     case SDL_SCANCODE_UP:
       nextDir = Math::Dir::UP;
@@ -115,55 +123,36 @@ Math::Dir getNextDir(const SDL_Scancode code, const Math::Dir currentDir) {
       nextDir = Math::Dir::LEFT;
       break;
     
-    default: ;
+    default:
+      nextDir = currentDir;
   }
   if (Math::opposite(nextDir) == currentDir) {
-    return currentDir;
-  } else {
-    return nextDir;
+    nextDir = currentDir;
   }
 }
 
-Pos getFoodPos(const std::deque<Pos> &snake) {
+void AppImpl::setFoodPos() {
   static std::random_device gen;
   static std::uniform_int_distribution<PosScalar> distX(0, GAME_SIZE.x - 1);
   static std::uniform_int_distribution<PosScalar> distY(0, GAME_SIZE.y - 1);
   
-  Pos food;
-  auto equalsFood = [&food](const Pos pos) {
+  auto equalsFood = [this](const Pos pos) {
     return food == pos;
   };
   do {
     food.x = distX(gen);
     food.y = distY(gen);
   } while (std::any_of(snake.cbegin(), snake.cend(), equalsFood));
-  
-  return food;
 }
 
-SDL_Rect scalePx(const Pos pos) {
-  return {
-    static_cast<int>(pos.x * PX_SIZE.x),
-    static_cast<int>(pos.y * PX_SIZE.y),
-    static_cast<int>(PX_SIZE.x),
-    static_cast<int>(PX_SIZE.y)
-  };
-}
-
-void renderSnake(SDL_Renderer *renderer, const std::deque<glm::ivec2> &snake) {
-  setColor(renderer, SNAKE_COLOR);
-  
-  std::vector<SDL_Rect> rects;
+void AppImpl::renderSnake() {
+  renderer.setColor(SNAKE_COLOR);
   for (auto p = snake.cbegin(); p != snake.cend(); ++p) {
-    rects.push_back(scalePx(*p));
+    SDL_RenderDrawPoint(renderer, p->x, p->y);
   }
-  
-  SDL_RenderFillRects(renderer, rects.data(), static_cast<int>(rects.size()));
 }
 
-void renderFood(SDL_Renderer *renderer, const Pos food) {
-  setColor(renderer, FOOD_COLOR);
-  
-  const SDL_Rect rect = scalePx(food);
-  SDL_RenderFillRect(renderer, &rect);
+void AppImpl::renderFood() {
+  renderer.setColor(FOOD_COLOR);
+  SDL_RenderDrawPoint(renderer, food.x, food.y);
 }
