1e35b7f05892556c708dec3237e7a6b45696e627
Kerndog73 <kerndog73@gmail.com>
2017-07-03 10:25:50 +0930

    Break game into classes
    The game is now much more flexible. I plan to add powerups and a two player mode.

--- Snake/Game/app impl.cpp
+++ Snake/Game/app impl.cpp
@@ -8,10 +8,10 @@
 
 #include "app impl.hpp"
 
-void AppImpl::init() {
-  const std::string atlasPath = Platform::getResDir() + "sprites.atlas";
-  const std::string imagePath = Platform::getResDir() + "sprites.png";
-  sheet = makeSpritesheet(atlasPath, imagePath);
+RenderManager::RenderManager(Platform::Renderer &renderer, const std::string &sheetPath)
+  : sheet(makeSpritesheet(sheetPath + ".atlas", sheetPath + ".png")),
+    texture(nullptr, &SDL_DestroyTexture),
+    renderer(renderer.get()) {
   const Image &image = sheet.getImage();
   texture.reset(SDL_CreateTexture(
     renderer.get(),
@@ -24,88 +24,12 @@ void AppImpl::init() {
   SDL_SetTextureBlendMode(texture.get(), SDL_BLENDMODE_BLEND);
 }
 
-bool AppImpl::input(const uint64_t) {
-  SDL_Event e;
-  while (SDL_PollEvent(&e)) {
-    switch (e.type) {
-      case SDL_QUIT:
-        return false;
-      case SDL_KEYDOWN:
-        setNextDir(e.key.keysym.scancode);
-        break;
-    }
-  }
-  return true;
+void RenderManager::reset() {
+  animProg = 0;
 }
 
-bool AppImpl::update(const uint64_t delta) {
+void RenderManager::update(const uint64_t delta) {
   animProg += delta;
-  if (animProg >= MILLI_PER_UPDATE) {
-    animProg = 0;
-    if (eating) {
-      setFoodPos();
-      eating = false;
-    } else {
-      snake.pop_back();
-    }
-    
-    snake.push_front((snake.front() + ToVec::conv(nextDir) + GAME_SIZE) % GAME_SIZE);
-    
-    if (snake.front() != food) {
-      for (auto s = snake.cbegin() + 1; s != snake.cend() - 1; ++s) {
-        if (*s == snake.front()) {
-          return false;
-        }
-      }
-    } else {
-      eating = true;
-    }
-    currentDir = nextDir;
-  }
-  return true;
-}
-
-void AppImpl::render(const uint64_t) {
-  renderer.clear(BACK_COLOR);
-  renderSnake();
-  renderFood();
-  renderer.present();
-}
-
-void AppImpl::setNextDir(const SDL_Scancode code) {
-  switch (code) {
-    case SDL_SCANCODE_UP:
-      nextDir = Math::Dir::UP;
-      break;
-    case SDL_SCANCODE_RIGHT:
-      nextDir = Math::Dir::RIGHT;
-      break;
-    case SDL_SCANCODE_DOWN:
-      nextDir = Math::Dir::DOWN;
-      break;
-    case SDL_SCANCODE_LEFT:
-      nextDir = Math::Dir::LEFT;
-      break;
-    
-    default:
-      nextDir = currentDir;
-  }
-  if (Math::opposite(nextDir) == currentDir) {
-    nextDir = currentDir;
-  }
-}
-
-void AppImpl::setFoodPos() {
-  static std::random_device gen;
-  static std::uniform_int_distribution<PosScalar> distX(0, GAME_SIZE.x - 1);
-  static std::uniform_int_distribution<PosScalar> distY(0, GAME_SIZE.y - 1);
-  
-  auto equalsFood = [this](const Pos pos) {
-    return food == pos;
-  };
-  do {
-    food = {distX(gen), distY(gen)};
-  } while (std::any_of(snake.cbegin(), snake.cend(), equalsFood));
 }
 
 SDL_Rect toSDL(const RectPx rect) {
@@ -121,17 +45,13 @@ SDL_Rect toTile(const Pos pos) {
   };
 }
 
-void AppImpl::renderSprite(
-  const Pos pos,
-  const std::string &name,
-  const double rotation = 0.0
-) {
+void RenderManager::render(const std::string &name, const Pos pos, const double rotation) {
   const double prog = static_cast<double>(animProg) / MILLI_PER_UPDATE;
   const std::string frame = std::to_string(Time::progToFrame(prog, NUM_FRAMES));
   const SDL_Rect srcRect = toSDL(sheet.getSprite(name + ' ' + frame));
   const SDL_Rect dstRect = toTile(pos);
   SDL_RenderCopyEx(
-    renderer.get(),
+    renderer,
     texture.get(),
     &srcRect,
     &dstRect,
@@ -139,6 +59,88 @@ void AppImpl::renderSprite(
     nullptr,
     SDL_FLIP_NONE
   );
+}
+
+Rat::Rat(const Pos pos)
+  : pos(pos) {}
+
+void Rat::reset(const Pos newPos) {
+  pos = newPos;
+  beingEaten = false;
+}
+
+Pos Rat::getPos() const {
+  return pos;
+}
+
+void Rat::eat() {
+  beingEaten = true;
+}
+
+void Rat::render(RenderManager &renderer) const {
+  if (not beingEaten) {
+    renderer.render("rat", pos);
+  }
+}
+
+Snake::Snake(const Pos center)
+  : positions({
+      {center.x - 1, center.y},
+      center,
+      {center.x + 1, center.y},
+      {center.x + 2, center.y}
+    }) {}
+
+void Snake::move(const Math::Dir newDir) {
+  if (Math::opposite(newDir) != currentDir) {
+    nextDir = newDir;
+  }
+}
+
+bool Snake::isEating(const Rat &rat) const {
+  return eating && positions.front() == rat.getPos();
+}
+
+bool Snake::colliding(const Pos pos, const bool withHead) const {
+  return std::any_of(
+    positions.cbegin() + !withHead,
+    positions.cend(),
+    [pos] (const Pos thisPos) {
+      return pos == thisPos;
+    }
+  );
+}
+
+void Snake::tryToEat(Rat &rat) {
+  if (head() == rat.getPos()) {
+    eating = true;
+    rat.eat();
+  }
+}
+
+bool Snake::isDead() const {
+  return dead;
+}
+
+Pos Snake::head() const {
+  return positions.front();
+}
+
+Pos Snake::tail() const {
+  return positions.back();
+}
+
+void Snake::update() {
+  if (eating) {
+    eating = false;
+  } else {
+    positions.pop_back();
+  }
+  positions.push_front((positions.front() + ToVec::conv(nextDir) + GAME_SIZE) % GAME_SIZE);
+  currentDir = nextDir;
+  if (colliding(head(), false) && head() != tail()) {
+    dead = true;
+  }
 }
 
 //returns a vector pointing from this to prev
@@ -181,7 +183,8 @@ std::string getBodySpriteTurnName(const Math::Dir prevToThisDir, const Math::Dir
   }
 }
 
-void AppImpl::renderSnakeBody(
+void renderBody(
+  RenderManager &renderer,
   const std::string &name,
   const Pos pos,
   const Pos prevToThisVec,
@@ -191,9 +194,9 @@ void AppImpl::renderSnakeBody(
   
   const Math::Dir prevToThisDir = FromVec::conv(prevToThisVec);
   const Math::Dir thisToNextDir = FromVec::conv(thisToNextVec);
-  renderSprite(
-    pos,
+  renderer.render(
     name + ' ' + getBodySpriteTurnName(prevToThisDir, thisToNextDir),
+    pos,
     getBodySpriteAngle(prevToThisDir)
   );
 }
@@ -205,37 +208,113 @@ std::string dirToString(const Math::Dir dir) {
   return NAMES[Math::ToNum<size_t>::conv(dir)];
 }
 
-void AppImpl::renderSnake() {
+void Snake::render(RenderManager &renderer) const {
   using FromVec = Math::FromVec<double, Math::Dir::RIGHT, Math::Dir::DOWN>;
   
   if (eating) {
-    renderSprite(snake.front(), "eat rat " + dirToString(currentDir));
+    renderer.render("eat rat " + dirToString(currentDir), head());
   } else {
-    renderSprite(snake.front(), "head", getBodySpriteAngle(currentDir));
+    renderer.render("head", head(), getBodySpriteAngle(currentDir));
   }
 
-  Pos nextPos = snake[2];
-  Pos thisToNextVec = getDirVec(snake[1], nextPos);
-  renderSnakeBody("head", snake[1], thisToNextVec, getDirVec(snake[0], snake[1]));
+  Pos nextPos = positions[2];
+  Pos thisToNextVec = getDirVec(positions[1], nextPos);
+  renderBody(renderer, "head", positions[1], thisToNextVec, getDirVec(positions[0], positions[1]));
   
-  for (auto b = snake.cbegin() + 2; b != snake.cend() - 2; ++b) {
+  for (auto b = positions.cbegin() + 2; b != positions.cend() - 2; ++b) {
     const Pos pos = nextPos;
     nextPos = *(b + 1);
     const Pos prevToThisVec = getDirVec(pos, nextPos);
-    renderSnakeBody("body", pos, prevToThisVec, thisToNextVec);
+    renderBody(renderer, "body", pos, prevToThisVec, thisToNextVec);
     thisToNextVec = prevToThisVec;
   }
   
-  const Pos prevToThisVec = getDirVec(nextPos, snake.back());
-  renderSnakeBody(eating ? "body" : "tail", nextPos, prevToThisVec, thisToNextVec);
+  const Pos prevToThisVec = getDirVec(nextPos, positions.back());
+  renderBody(renderer, eating ? "body" : "tail", nextPos, prevToThisVec, thisToNextVec);
   thisToNextVec = prevToThisVec;
 
   const double tailAngle = getBodySpriteAngle(FromVec::conv(thisToNextVec));
-  renderSprite(snake.back(), eating ? "tail grow" : "tail", tailAngle);
+  renderer.render(eating ? "tail grow" : "tail", tail(), tailAngle);
 }
 
-void AppImpl::renderFood() {
-  if (not eating) {
-    renderSprite(food, "rat");
+AppImpl::AppImpl()
+  : SDLApp(WINDOW_DESC, true),
+    renderMan(renderer, Platform::getResDir() + "sprites"),
+    snake(GAME_SIZE / 2),
+    rat({0, GAME_SIZE.y / 2}) {}
+
+void AppImpl::init() {
+  
+}
+
+bool AppImpl::input(const uint64_t) {
+  SDL_Event e;
+  while (SDL_PollEvent(&e)) {
+    switch (e.type) {
+      case SDL_QUIT:
+        return false;
+      case SDL_KEYDOWN:
+        snakeInput(e.key.keysym.scancode);
+        break;
+    }
   }
+  return true;
+}
+
+bool AppImpl::update(const uint64_t delta) {
+  animProg += delta;
+  renderMan.update(delta);
+  if (animProg >= MILLI_PER_UPDATE) {
+    animProg = 0;
+    renderMan.reset();
+    if (snake.isEating(rat)) {
+      rat.reset(getFoodPos());
+    }
+    
+    snake.update();
+    snake.tryToEat(rat);
+    
+    if (snake.isDead()) {
+      return false;
+    }
+  }
+  return true;
+}
+
+void AppImpl::render(const uint64_t) {
+  renderer.clear(BACK_COLOR);
+  snake.render(renderMan);
+  rat.render(renderMan);
+  renderer.present();
+}
+
+void AppImpl::snakeInput(const SDL_Scancode code) {
+  switch (code) {
+    case SDL_SCANCODE_UP:
+      snake.move(Math::Dir::UP);
+      break;
+    case SDL_SCANCODE_RIGHT:
+      snake.move(Math::Dir::RIGHT);
+      break;
+    case SDL_SCANCODE_DOWN:
+      snake.move(Math::Dir::DOWN);
+      break;
+    case SDL_SCANCODE_LEFT:
+      snake.move(Math::Dir::LEFT);
+    default: ;
+  }
+}
+
+Pos AppImpl::getFoodPos() const {
+  static std::random_device gen;
+  static std::uniform_int_distribution<PosScalar> distX(0, GAME_SIZE.x - 1);
+  static std::uniform_int_distribution<PosScalar> distY(0, GAME_SIZE.y - 1);
+  
+  Pos newPos;
+  
+  do {
+    newPos = {distX(gen), distY(gen)};
+  } while (snake.colliding(newPos));
+  
+  return newPos;
 }
--- Snake/Game/app impl.hpp
+++ Snake/Game/app impl.hpp
@@ -42,42 +42,85 @@ const glm::vec2 TILE_SIZE = {
   static_cast<float>(WINDOW_DESC.size.y) / GAME_SIZE.y
 };
 
-class AppImpl final : public SDLApp {
+class RenderManager {
 public:
-  AppImpl()
-    : SDLApp(WINDOW_DESC, true),
-      texture(nullptr, &SDL_DestroyTexture) {
-    const Pos center = GAME_SIZE / 2;
-    snake = {
-      {center.x - 1, center.y},
-      center,
-      {center.x + 1, center.y},
-      {center.x + 2, center.y}
-    };
-  }
+  RenderManager(Platform::Renderer &, const std::string &);
+  
+  void reset();
+  
+  void update(uint64_t);
+  void render(const std::string &, Pos, double = 0.0);
   
 private:
   Spritesheet sheet;
   std::unique_ptr<SDL_Texture, decltype(&SDL_DestroyTexture)> texture;
+  SDL_Renderer *renderer;
   uint64_t animProg = 0;
-  std::deque<Pos> snake;
-  Pos food = {0, GAME_SIZE.y / 2};
+};
+
+class InputManager {
+public:
+  InputManager() = default;
+  
+  
+};
+
+class Rat {
+public:
+  explicit Rat(Pos);
+  
+  void reset(Pos);
+  Pos getPos() const;
+  void eat();
+  
+  void render(RenderManager &) const;
+  
+private:
+  Pos pos;
+  bool beingEaten = false;
+};
+
+class Snake {
+public:
+  explicit Snake(Pos);
+  
+  void move(Math::Dir);
+  bool isEating(const Rat &) const;
+  bool colliding(Pos, bool = true) const;
+  void tryToEat(Rat &);
+  bool isDead() const;
+  
+  Pos head() const;
+  Pos tail() const;
+  
+  void update();
+  void render(RenderManager &) const;
+  
+private:
+  std::deque<Pos> positions;
   Math::Dir currentDir = Math::Dir::LEFT;
   Math::Dir nextDir = Math::Dir::LEFT;
   bool eating = false;
+  bool dead = false;
+};
+
+class AppImpl final : public SDLApp {
+public:
+  AppImpl();
+  
+private:
+  RenderManager renderMan;
+  uint64_t animProg = 0;
+  Snake snake;
+  Rat rat;
 
   void init() override;
   bool input(uint64_t) override;
   bool update(uint64_t) override;
   void render(uint64_t) override;
   
-  void setNextDir(SDL_Scancode);
-  void setFoodPos();
-  void renderSprite(Pos, const std::string &, double);
-  
-  void renderSnakeBody(const std::string &, Pos, Pos, Pos);
-  void renderSnake();
-  void renderFood();
+  Pos getFoodPos() const;
+  void snakeInput(SDL_Scancode);
 };
 
 #endif
